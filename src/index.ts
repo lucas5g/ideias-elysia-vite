5dd9a8c15fab082ec0fc4627dcdd6b5071d8c40d
lucas.assuncao <lucas.assuncao@defensoria.mg.def.br>
2025-09-17 18:22:43 -0300

    add swagger

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,18 @@
-import { projectRoute } from '@/project/project.route'
-import { projetcRoute } from '@/projetc/projetc.route'
-import { userRoute } from '@/user/user.route'
 import { Elysia } from 'elysia';
 import cors from '@elysiajs/cors';
 import { phraseRoute } from '@/phrase/phrase.route';
+import swagger from '@elysiajs/swagger';
+import { env } from '@/utils/env';
 
 const app = new Elysia()
   .onError((err) => console.error(err))
   .use(cors())
-  .get('/', () => 'Hello Elysia')
+  .use(swagger())
+  .get('/', ({ redirect }) => redirect('/swagger'))
   .use(phraseRoute)
-  .use(userRoute)
-  .use(projetcRoute)
-  .use(projectRoute)
   .listen(3000);
 
 console.debug(
-  `🦊 Elysia is running at http://${app.server?.hostname}:${app.server?.port}`
+  `🦊 Elysia is running at ${env.BASE_URL_API}`
 );
 
