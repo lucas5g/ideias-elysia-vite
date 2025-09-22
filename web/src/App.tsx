75063c392469a0c0f7859c74d0fde16324df06c7
lucas.sousa <lucas.sousa@codificar.com.br>
2025-09-22 19:33:00 -0300

    fix import test

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -3,7 +3,6 @@ import { Layout } from '@/components/Layout';
 import { BrowserRouter, Route, Routes } from 'react-router';
 import { Translate } from '@/pages/translate/Index';
 import { Tests } from '@/pages/tests';
-import { TranslateTest } from '@/pages/translate/Test';
 
 export function App() {
   return (
@@ -12,8 +11,6 @@ export function App() {
         <Route element={<Layout />}>
           <Route path="/" element={<Translate />} />
           <Route path="/translate" element={<Translate />} />
-          <Route path="/translate/test" element={<TranslateTest />} />
-
           <Route path="/tests" element={<Tests />} />
 
         </Route>
