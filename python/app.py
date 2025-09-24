60a944514632e34b62007aa30ceed5b8a76761e0
lucas.assuncao <lucas.assuncao@defensoria.mg.def.br>
2025-09-24 19:14:36 -0300

    add python

--- python/app.py
+++ python/app.py
@@ -1,22 +1,48 @@
 import streamlit as st  
-from helper import get_phrases
+from helper import get_phrases, create_phrase
 
-st.set_page_config(page_title="Hello", page_icon=":tada:", layout="wide")
-search = st.text_input('', placeholder="Portuguese, English and Tags")
-st.button('Search')
+st.set_page_config(page_title="Ideias", page_icon=":idea:", layout="wide")
+st.header('Ideias')
 
+list, form = st.tabs(['List', 'Form'])
 
-for row in get_phrases(search=search):
-  tags = ''
-  for tag in row['tags']:
-    tags += f'`{tag}` '
+with form:
+  # with st.form('Form'):   
+  payload = {}
+  audio = None
+  payload['type'] = st.selectbox('Type', ('TRANSLATION', 'NEGATIVE', 'INTERROGATIVE', 'STORY'))
+  if payload['type'] == 'TRANSLATION':
+    payload['portuguese'] = st.text_input('Portuguese', placeholder="Portuguese", key='portuguese' )    
   
-  st.markdown(f'''
-    ###### {row["english"]}     
-    {row["portuguese"]}
+  if payload['type'] == 'NEGATIVE' or payload['type'] == 'INTERROGATIVE':
+    audio = st.file_uploader('Audio', type=['ogg', 'mp3'])             
+      
+  payload['tags'] =  [st.text_input('Tags', placeholder="Tags", key='tags')]
+  
+  if st.button('Save'):  
+    res = create_phrase(payload, audio)
+    if 'id' in res:
+      st.success('Phrase created')
+    else:      
+      st.warning(res['message'])
+          
+
+with list:
+  search = st.text_input('test', placeholder="Portuguese, English and Tags", )
+  st.button('Search')
+  
+
+  for row in get_phrases(search=search):
+    tags = ''
+    for tag in row['tags']:
+      tags += f'`{tag}` '
     
-    {tags}
-  ''')
-  
-  st.audio(row['audio'])
-  st.markdown('---')
+    st.markdown(f'''
+      ###### {row["english"]}     
+      {row["portuguese"]}
+      
+      {tags}
+    ''')
+    
+    st.audio(row['audio'])
+    st.divider()
--- python/helper.py
+++ python/helper.py
@@ -1,9 +1,24 @@
 import requests 
 import pandas as pd 
+import streamlit as st
 
+# API_URL = 'https://api.dizelequefez.com.br'
+API_URL = 'http://localhost:3000'
 
 
 def get_phrases(search=None):  
-  res = requests.get(f'https://api.dizelequefez.com.br/phrases?search={search or ""}')
+  res = requests.get(f'{API_URL}/phrases?search={search}')
   return res.json()
   
+def create_phrase(payload, audio): 
+  
+  if audio:
+    # payload['tags'] = 
+    st.write(payload)
+    res = requests.post(f'{API_URL}/phrases', data=payload, files={'audio': audio})
+    print(res.text)
+    return 'teste'
+    # return res.json()
+  
+  res = requests.post(f'{API_URL}/phrases', json=payload)
+  return res.json()
