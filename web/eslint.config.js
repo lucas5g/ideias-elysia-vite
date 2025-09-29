f8f7ca70c9aa6a213c410a67a11b5b4c6fddf254
lucas.sousa <lucas.sousa@codificar.com.br>
2025-09-29 18:37:53 -0300

    remove alert react-hooks/extrative-deps

--- web/eslint.config.js
+++ web/eslint.config.js
@@ -23,6 +23,7 @@ export default tseslint.config([
       'react-refresh/only-export-components': 'off',
       '@typescript-eslint/no-empty-interface': 'off',
       '@typescript-eslint/no-empty-object-type': 'off',
+      'react-hooks/exhaustive-deps': 'off',
 
     }
   },
