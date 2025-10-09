f9eb82cd1679a053d282fc28256e4b772b844122
lucas.assuncao <lucas.assuncao@defensoria.mg.def.br>
2025-10-09 17:49:23 -0300

    button icon

--- web2/src/components/Form.tsx
+++ web2/src/components/Form.tsx
@@ -3,6 +3,7 @@ import { Input } from "./Input";
 import { useNavigate, useSearchParams } from 'react-router';
 import { useEffect } from 'react';
 import { api } from '@/utils/api';
+import { TrashIcon } from '@phosphor-icons/react'
 
 interface Props {
   fields: FieldInterface
@@ -32,25 +33,43 @@ export function Form({ fields, uri }: Readonly<Props>) {
 
   }, [id])
 
-  function handleReset(){
+  function handleReset() {
     headers.forEach((header) => {
       document.getElementById(header)?.setAttribute('value', '')
     })
 
-
-
     navigate(uri)
+  }
+
+  async function handleDelete() {
+    const confirm = window.confirm('Are you sure?')
+
+    if (!confirm) {
+      return
+    }
+    await api.delete(`${uri}/${id}`)
+    handleReset()
   }
 
   return (
     <form className="card">
-
-      <h1>
-        Form
-      </h1>
-      <button>
-        delete
-      </button>
+      <div className='row justify-between border-b border-gray-600'>
+        <h2>
+          Form
+        </h2>
+        {id &&
+          <TrashIcon 
+            size={20} 
+            className='cursor-pointer rounded-full hover:bg-red-500' 
+            onClick={handleDelete} 
+            // color='white'
+            />
+          // <button
+          //   type='button'
+          //   className='bg-red-500 hover:bg-red-600 hover:border hover:border-red-700 w-8 h-8 rounded-full flex items-center justify-center'>
+          // </button>
+        }
+      </div>
       {Object.keys(fields).map((field) => (
         <Input key={field} name={field} {...fields[field]} />
       ))}
@@ -59,19 +78,15 @@ export function Form({ fields, uri }: Readonly<Props>) {
         <button className="button-primary" type="submit">
           {id ? 'Update' : 'Create'}
         </button>
-        <button 
-          className="button-secondary" 
+        <button
+          className="button-secondary"
           type="reset"
           onClick={handleReset}
-          >
+        >
           Cancel
         </button>
       </div>
-      {id &&
-        <button type="button" className="button-delete">
-          Delete
-        </button>
-      }
+
     </form>
   )
 }
