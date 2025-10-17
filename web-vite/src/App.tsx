fdf75fe62b698af6037c70a51c56604a083e4403
lucas.sousa <lucas.sousa@codificar.com.br>
2025-10-17 17:59:55 -0300

    test release

--- web-vite/src/App.tsx
+++ web-vite/src/App.tsx
@@ -1,29 +1,26 @@
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router'
 import { Food } from './pages/Food'
-import { Diet } from '@/pages/Diet'
 import { Layout } from '@/components/Layout'
 import { Modelo } from '@/pages/modelo/Modelo'
 import { Login } from '@/pages/modelo/Login'
 import { User } from './pages/Users'
-import { DietNew } from './pages/Diet-new'
 import { Audiobook } from './pages/Audiobook'
-import { DietNew2 } from '@/pages/Diet-new-2'
+import { Diet } from '@/pages/Diet'
+import { version } from '../package.json'
 export function App() {
-
+  console.log(`Running app version: ${version}`)
   return (
     <BrowserRouter>
       <Routes>
         <Route element={<Layout />}>
           <Route path="/" element={<Food />} />
           <Route path="/foods" element={<Food />} />
-          <Route path="/diets" element={<Diet />} />
-          <Route path="/diets-new" element={<DietNew />} />
           <Route path='/users' element={<User />} />
         </Route>
+        <Route path="/diets" element={<Diet />} />
         <Route path="/modelos" element={<Modelo />} />
         <Route path="/modelos/login" element={<Login />} />
         <Route path="/audiobooks" element={<Audiobook />} />
-        <Route path="/diets-new-2" element={<DietNew2 />} />
 
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
