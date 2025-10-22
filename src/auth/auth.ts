d244d0e77d173379ad533246bcf707792bcbf669
lucas.assuncao <lucas.assuncao@defensoria.mg.def.br>
2025-10-22 18:10:12 -0300

    comentei auth.ts

--- src/auth/auth.ts
+++ src/auth/auth.ts
@@ -1,17 +1,17 @@
-import { env } from '@/utils/env';
-import { prisma } from '@/utils/prisma';
-import { betterAuth } from 'better-auth';
-import { prismaAdapter } from 'better-auth/adapters/prisma'; 
-export const auth = betterAuth({
+// import { env } from '@/utils/env';
+// import { prisma } from '@/utils/prisma';
+// import { betterAuth } from 'better-auth';
+// import { prismaAdapter } from 'better-auth/adapters/prisma'; 
+// export const auth = betterAuth({
 
-  database: prismaAdapter(prisma, {
-    provider: 'sqlite',
-  }),
-  socialProviders: {
-    google: {
-      clientId: env.GOOGLE_CLIENT_ID,
-      clientSecret: env.GOOGLE_CLIENT_SECRET,
-    },
-  },
-  basePath:'/api'
-});
+//   database: prismaAdapter(prisma, {
+//     provider: 'sqlite',
+//   }),
+//   socialProviders: {
+//     google: {
+//       clientId: env.GOOGLE_CLIENT_ID,
+//       clientSecret: env.GOOGLE_CLIENT_SECRET,
+//     },
+//   },
+//   basePath:'/api'
+// });
