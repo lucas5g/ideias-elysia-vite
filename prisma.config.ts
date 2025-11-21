d6273c844e1d1adc365bba8b7a1be3999c7475ee
lucas.sousa <lucas.sousa@codificar.com.br>
2025-11-21 20:47:53 -0300

    teste prisma

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,12 +1,12 @@
-import { defineConfig, env } from 'prisma/config'
+import { defineConfig } from 'prisma/config'
 import 'dotenv/config'
 export default defineConfig({
   migrations: {
     seed: "bun prisma/seed.ts"
   },
-  datasource: {
-    url: env('DATABASE_URL')
-  },
+  // datasource: {
+  //   url: env('DATABASE_URL')
+  // },
   // adapter:() => ({
   //   type: 'postgresql',
   //   url: process.env.DATABASE_URL || '',
